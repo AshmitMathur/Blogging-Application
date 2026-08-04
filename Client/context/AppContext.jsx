4843cafaf046697d944334a582cc4f3a66325cbb
AshmitMathur <mathurashmit06@gmail.com>
2026-08-04 12:02:46 +0530

    To deploy the project on Vercel

--- Client/context/AppContext.jsx
+++ Client/context/AppContext.jsx
@@ -23,7 +23,6 @@ export const AppProvider = ({children}) => {
     const fetchBlogs = async()=> {
         try {
            const {data} =  await axios.get("/api/blog/all");
-                   console.log(data);
            data.success ? setBlogs(data.blogs) : toast.error(data.message)
         } catch (error) {
             toast.error(error.message);
