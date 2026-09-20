f607cacbd5ded6fe65e822bee687e011b7e68dcf
AshmitMathur <mathurashmit06@gmail.com>
2026-09-20 21:55:08 +0530

    Improved Structure of Routes

--- Server/routes/BookMarkRoutes.js
+++ Server/routes/BookMarkRoutes.js
@@ -4,15 +4,9 @@ import {
     getBookmarkStatus,
     getMyBookmarks,
 } from "../controllers/BookMarkController.js";
-
 import userAuth from "../middlewares/userAuth.js";
-
 const router = express.Router();
-
 router.post("/:blogId", userAuth, toggleBookmark);
-
 router.get("/status/:blogId", userAuth, getBookmarkStatus);
-
 router.get("/my", userAuth, getMyBookmarks);
-
 export default router;
--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,10 +1,7 @@
 import express from "express";
 import { getUserProfile, updateProfile  } from "../controllers/UserController.js"
 import userAuth from "../middlewares/userAuth.js";
-
 const userRouter = express.Router();
-
 userRouter.put("/update", userAuth, updateProfile);
 userRouter.get("/:username", getUserProfile);
-
 export default userRouter;
